ed69d3c1da46198b62e37ee78573b4b2882330f3
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-09-26 16:28:30 -0400

    added code to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
-var newDate=  moment().format("MM/DD/YYYY");
-$("#currentDay").html (newDate);
+// Display today's day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+
+    
+
+// saveBtn Button
+
+    $(".saveBtn").click(function(){
+      var blockText = $(this).siblings(".description").val()
+      var data = $(this).parent().attr("id");
+      
+      localStorage.setItem(data, blockText);
+  });
+
+  
+///localstorage
+    $("#nineAm .description").val(localStorage.getItem("nineAm"));
+    $("#tenAm .description").val(localStorage.getItem("tenAm"));
+    $("#elevenAm .description").val(localStorage.getItem("elevenAm"));
+    $("#twelvePm .description").val(localStorage.getItem("twelvePm"));
+    $("#onePm .description").val(localStorage.getItem("onePm"));
+    $("#twoPm .description").val(localStorage.getItem("twoPm"));
+    $("#threePm .description").val(localStorage.getItem("threePm"));
+    $("#fourPm .description").val(localStorage.getItem("fourPm"));
+    $("#fivePm .description").val(localStorage.getItem("fivePm"));
+  
+ 
 
